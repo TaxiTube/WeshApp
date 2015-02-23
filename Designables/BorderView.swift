a9359603a925db67f6a3a6dc2ace76df4d9af1a2
rabzu <z.kakabadze@gmail.com>
2015-02-23 20:11:28 +0000

    line made pale

--- Designables/BorderView.swift
+++ Designables/BorderView.swift
@@ -18,10 +18,13 @@ class BorderView: UIView {
     
     @IBInspectable var lineWidth: CGFloat = 0.0
     
+    let grey80 = UIColor(white:0.80, alpha:1)
+
+   
     
     override func drawRect(rect:CGRect) {
-        let grey80 = UIColor(white:0.80, alpha:1)
         
+            let colour =  UIColorFromRGB(0xf7f7f7)
         let context = UIGraphicsGetCurrentContext()
     
         if top {
@@ -45,8 +48,17 @@ class BorderView: UIView {
         }
         
         
-        CGContextSetStrokeColorWithColor(context, grey80.CGColor)
+        CGContextSetStrokeColorWithColor(context, colour.CGColor)
         CGContextSetLineWidth(context, 1.0);
         CGContextStrokePath(context);
+    }
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
 }
